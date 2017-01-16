c46f37f7c345c3db04b027dcf638f6db5890dbd3
UltraDark <mike@ultradark.com>
2017-01-16 17:59:04 +0000

    Add files via upload

---
+++ Generated/Windows_Wix_Installer/~WindowsWixInstaller.0012.0014.Simulator.RegistryKey.StateDisplay.c
@@ -0,0 +1,97 @@
+
+#include "~WindowsWixInstaller.0000.0000.Header.h"
+
+
+/* Simulator Registry Key Display State */
+
+void adm_sim_display_state_registry_key_generating(
+    adm_pool_target_registry_key_typ * pool_target)
+{
+    #if ADM_PROBE
+    adm_probe_enter(ADM_PROBE_GROUP_SIMULATOR, "adm_sim_display_state_registry_key_generating");
+    #endif
+
+    if (
+        adm_sim_trace.all &&
+        adm_sim_trace.states) {
+
+        printf(NEWLINE);
+        printf("STATE (1:1275:42) - Registry_Key[");
+        printf("%d", (pool_target->ent_registry_key)->identity);
+        printf("] ");
+        printf("Generating");
+        printf(" {");
+        printf("%s", adm_sim_find_event_name(ADM_SIM_INDEX_ENTITY_MATRIX_INSTALLER_REGISTRY_KEY, pool_target->evente));
+        printf(" -> ");
+        printf("%s", adm_sim_status_registry_key[(pool_target->ent_registry_key)->status]);
+        printf("} (");
+        printf(")");
+        printf(NEWLINE);
+    }
+
+    #if ADM_PROBE
+    adm_probe_leave(ADM_PROBE_GROUP_SIMULATOR, "adm_sim_display_state_registry_key_generating");
+    #endif
+}
+
+void adm_sim_display_state_registry_key_generating_registry_value(
+    adm_pool_target_registry_key_typ * pool_target)
+{
+    #if ADM_PROBE
+    adm_probe_enter(ADM_PROBE_GROUP_SIMULATOR, "adm_sim_display_state_registry_key_generating_registry_value");
+    #endif
+
+    if (
+        adm_sim_trace.all &&
+        adm_sim_trace.states) {
+
+        printf(NEWLINE);
+        printf("STATE (1:1293:42) - Registry_Key[");
+        printf("%d", (pool_target->ent_registry_key)->identity);
+        printf("] ");
+        printf("Generating_Registry_Value");
+        printf(" {");
+        printf("%s", adm_sim_find_event_name(ADM_SIM_INDEX_ENTITY_MATRIX_INSTALLER_REGISTRY_KEY, pool_target->evente));
+        printf(" -> ");
+        printf("%s", adm_sim_status_registry_key[(pool_target->ent_registry_key)->status]);
+        printf("} (");
+        printf(")");
+        printf(NEWLINE);
+    }
+
+    #if ADM_PROBE
+    adm_probe_leave(ADM_PROBE_GROUP_SIMULATOR, "adm_sim_display_state_registry_key_generating_registry_value");
+    #endif
+}
+
+void adm_sim_display_state_registry_key_idle(
+    adm_pool_target_registry_key_typ * pool_target)
+{
+    #if ADM_PROBE
+    adm_probe_enter(ADM_PROBE_GROUP_SIMULATOR, "adm_sim_display_state_registry_key_idle");
+    #endif
+
+    if (
+        adm_sim_trace.all &&
+        adm_sim_trace.states) {
+
+        printf(NEWLINE);
+        printf("STATE (1:1311:42) - Registry_Key[");
+        printf("%d", (pool_target->ent_registry_key)->identity);
+        printf("] ");
+        printf("Idle");
+        printf(" {");
+        printf("%s", adm_sim_find_event_name(ADM_SIM_INDEX_ENTITY_MATRIX_INSTALLER_REGISTRY_KEY, pool_target->evente));
+        printf(" -> ");
+        printf("%s", adm_sim_status_registry_key[(pool_target->ent_registry_key)->status]);
+        printf("} (");
+        printf(")");
+        printf(NEWLINE);
+    }
+
+    #if ADM_PROBE
+    adm_probe_leave(ADM_PROBE_GROUP_SIMULATOR, "adm_sim_display_state_registry_key_idle");
+    #endif
+}
+
+/* Copyright (C) 2017 Dark Matter Systems Ltd. All rights reserved. */
---
+++ Generated/Windows_Wix_Installer/~WindowsWixInstaller.0012.0016.Simulator.Feature.StateDisplay.c
@@ -0,0 +1,187 @@
+
+#include "~WindowsWixInstaller.0000.0000.Header.h"
+
+
+/* Simulator Feature Display State */
+
+void adm_sim_display_state_feature_display_heading(
+    adm_pool_target_feature_typ * pool_target)
+{
+    #if ADM_PROBE
+    adm_probe_enter(ADM_PROBE_GROUP_SIMULATOR, "adm_sim_display_state_feature_display_heading");
+    #endif
+
+    if (
+        adm_sim_trace.all &&
+        adm_sim_trace.states) {
+
+        printf(NEWLINE);
+        printf("STATE (1:1424:42) - Feature[");
+        printf("%d", (pool_target->ent_feature)->identity);
+        printf("] ");
+        printf("Display_Heading");
+        printf(" {");
+        printf("%s", adm_sim_find_event_name(ADM_SIM_INDEX_ENTITY_MATRIX_INSTALLER_FEATURE, pool_target->evente));
+        printf(" -> ");
+        printf("%s", adm_sim_status_feature[(pool_target->ent_feature)->status]);
+        printf("} (");
+        printf(")");
+        printf(NEWLINE);
+    }
+
+    #if ADM_PROBE
+    adm_probe_leave(ADM_PROBE_GROUP_SIMULATOR, "adm_sim_display_state_feature_display_heading");
+    #endif
+}
+
+void adm_sim_display_state_feature_generating(
+    adm_pool_target_feature_typ * pool_target)
+{
+    #if ADM_PROBE
+    adm_probe_enter(ADM_PROBE_GROUP_SIMULATOR, "adm_sim_display_state_feature_generating");
+    #endif
+
+    if (
+        adm_sim_trace.all &&
+        adm_sim_trace.states) {
+
+        printf(NEWLINE);
+        printf("STATE (1:1449:42) - Feature[");
+        printf("%d", (pool_target->ent_feature)->identity);
+        printf("] ");
+        printf("Generating");
+        printf(" {");
+        printf("%s", adm_sim_find_event_name(ADM_SIM_INDEX_ENTITY_MATRIX_INSTALLER_FEATURE, pool_target->evente));
+        printf(" -> ");
+        printf("%s", adm_sim_status_feature[(pool_target->ent_feature)->status]);
+        printf("} (");
+        printf(")");
+        printf(NEWLINE);
+    }
+
+    #if ADM_PROBE
+    adm_probe_leave(ADM_PROBE_GROUP_SIMULATOR, "adm_sim_display_state_feature_generating");
+    #endif
+}
+
+void adm_sim_display_state_feature_generating_feature(
+    adm_pool_target_feature_typ * pool_target)
+{
+    #if ADM_PROBE
+    adm_probe_enter(ADM_PROBE_GROUP_SIMULATOR, "adm_sim_display_state_feature_generating_feature");
+    #endif
+
+    if (
+        adm_sim_trace.all &&
+        adm_sim_trace.states) {
+
+        printf(NEWLINE);
+        printf("STATE (1:1519:42) - Feature[");
+        printf("%d", (pool_target->ent_feature)->identity);
+        printf("] ");
+        printf("Generating_Feature");
+        printf(" {");
+        printf("%s", adm_sim_find_event_name(ADM_SIM_INDEX_ENTITY_MATRIX_INSTALLER_FEATURE, pool_target->evente));
+        printf(" -> ");
+        printf("%s", adm_sim_status_feature[(pool_target->ent_feature)->status]);
+        printf("} (");
+        printf(")");
+        printf(NEWLINE);
+    }
+
+    #if ADM_PROBE
+    adm_probe_leave(ADM_PROBE_GROUP_SIMULATOR, "adm_sim_display_state_feature_generating_feature");
+    #endif
+}
+
+void adm_sim_display_state_feature_finishing_product_feature(
+    adm_pool_target_feature_typ * pool_target)
+{
+    #if ADM_PROBE
+    adm_probe_enter(ADM_PROBE_GROUP_SIMULATOR, "adm_sim_display_state_feature_finishing_product_feature");
+    #endif
+
+    if (
+        adm_sim_trace.all &&
+        adm_sim_trace.states) {
+
+        printf(NEWLINE);
+        printf("STATE (1:1565:42) - Feature[");
+        printf("%d", (pool_target->ent_feature)->identity);
+        printf("] ");
+        printf("Finishing_Product_Feature");
+        printf(" {");
+        printf("%s", adm_sim_find_event_name(ADM_SIM_INDEX_ENTITY_MATRIX_INSTALLER_FEATURE, pool_target->evente));
+        printf(" -> ");
+        printf("%s", adm_sim_status_feature[(pool_target->ent_feature)->status]);
+        printf("} (");
+        printf(")");
+        printf(NEWLINE);
+    }
+
+    #if ADM_PROBE
+    adm_probe_leave(ADM_PROBE_GROUP_SIMULATOR, "adm_sim_display_state_feature_finishing_product_feature");
+    #endif
+}
+
+void adm_sim_display_state_feature_generating_component(
+    adm_pool_target_feature_typ * pool_target)
+{
+    #if ADM_PROBE
+    adm_probe_enter(ADM_PROBE_GROUP_SIMULATOR, "adm_sim_display_state_feature_generating_component");
+    #endif
+
+    if (
+        adm_sim_trace.all &&
+        adm_sim_trace.states) {
+
+        printf(NEWLINE);
+        printf("STATE (1:1589:42) - Feature[");
+        printf("%d", (pool_target->ent_feature)->identity);
+        printf("] ");
+        printf("Generating_Component");
+        printf(" {");
+        printf("%s", adm_sim_find_event_name(ADM_SIM_INDEX_ENTITY_MATRIX_INSTALLER_FEATURE, pool_target->evente));
+        printf(" -> ");
+        printf("%s", adm_sim_status_feature[(pool_target->ent_feature)->status]);
+        printf("} (");
+        printf(")");
+        printf(NEWLINE);
+    }
+
+    #if ADM_PROBE
+    adm_probe_leave(ADM_PROBE_GROUP_SIMULATOR, "adm_sim_display_state_feature_generating_component");
+    #endif
+}
+
+void adm_sim_display_state_feature_finishing_component_feature(
+    adm_pool_target_feature_typ * pool_target)
+{
+    #if ADM_PROBE
+    adm_probe_enter(ADM_PROBE_GROUP_SIMULATOR, "adm_sim_display_state_feature_finishing_component_feature");
+    #endif
+
+    if (
+        adm_sim_trace.all &&
+        adm_sim_trace.states) {
+
+        printf(NEWLINE);
+        printf("STATE (1:1635:42) - Feature[");
+        printf("%d", (pool_target->ent_feature)->identity);
+        printf("] ");
+        printf("Finishing_Component_Feature");
+        printf(" {");
+        printf("%s", adm_sim_find_event_name(ADM_SIM_INDEX_ENTITY_MATRIX_INSTALLER_FEATURE, pool_target->evente));
+        printf(" -> ");
+        printf("%s", adm_sim_status_feature[(pool_target->ent_feature)->status]);
+        printf("} (");
+        printf(")");
+        printf(NEWLINE);
+    }
+
+    #if ADM_PROBE
+    adm_probe_leave(ADM_PROBE_GROUP_SIMULATOR, "adm_sim_display_state_feature_finishing_component_feature");
+    #endif
+}
+
+/* Copyright (C) 2017 Dark Matter Systems Ltd. All rights reserved. */
